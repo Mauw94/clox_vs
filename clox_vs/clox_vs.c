ec3086934e3d4c0b303bfb7c37d0f1da54e7c18f
Maurits Seelen <mauritsseelen@hotmail.com>
2024-03-20 14:26:04 +0100

    binary operators

--- clox_vs/clox_vs.c
+++ clox_vs/clox_vs.c
@@ -11,9 +11,22 @@ int main(int argc, const char* argv[])
 	Chunk chunk;
 	initChunk(&chunk);
 
-	int constant = addConstant(&chunk, 1.2);
+	int constant = addConstant(&chunk, 3);
 	writeChunk(&chunk, OP_CONSTANT, 123);
 	writeChunk(&chunk, constant, 123);
+
+	constant = addConstant(&chunk, 5);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+
+	writeChunk(&chunk, OP_ADD, 123);
+
+	constant = addConstant(&chunk, 2);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+
+	writeChunk(&chunk, OP_DIVIDE, 123);
+	writeChunk(&chunk, OP_NEGATE, 123);
 
 	writeChunk(&chunk, OP_RETURN, 123);
 
